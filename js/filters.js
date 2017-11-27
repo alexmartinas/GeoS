68e59aa8a4f1e755f7b4dc36ddd7e3b05bcfbd23
martynas5991 <alex_martynas@yahoo.com>
2017-11-27 11:22:22 +0200

    Added js to add or remove filters

--- js/filters.js
+++ js/filters.js
@@ -15,12 +15,14 @@ function collapseFilter(event){
 var source = [];
 var area = [];
 var category = [];
-var time = [];
+var posted = [];
 var dimension = [];
 
 function addFilter(event){
     target = event.target;
+
     if (target.tagName === "INPUT"){
+        console.log(target);
         var filterName = target.id[0].toUpperCase() + target.id.slice(1);
         var filter = target.getAttribute('data-filter');
 
@@ -33,9 +35,10 @@ function addFilter(event){
                 category.push(filterName);
             } else if (filter === "Dimension"){
                 dimension.push(filterName);
-            } else if (filter === "Time") {
-                time.push(filterName);
+            } else if (filter === "Posted") {
+                posted.push(filterName);
             }
+            addSelectedFilter(filter, filterName);
         } else {
             var index;
             if (filter === 'Source'){
@@ -50,13 +53,61 @@ function addFilter(event){
             } else if (filter === "Dimension"){
                 index = source.indexOf(filterName);
                 dimension.splice(index, 1);
-            } else if (filter === "Time") {
+            } else if (filter === "Posted") {
                 index = source.indexOf(filterName);
-                time.splice(index, 1);
+                posted.splice(index, 1);
             }
+            removeSelectedFilter(filter + "-" + filterName);
         }
 
-        console.log(source,area,category,time,dimension);
+        console.log(source,area,category,posted,dimension);
 
     }
+}
+
+function addSelectedFilter(type,filter){
+    var container = document.getElementById("selectedFilters");
+    var div = document.createElement("DIV");
+    div.className = "selected-filter";
+    div.id = type + "-" + filter;
+    div.textContent = filter;
+    var i = document.createElement('I');
+    i.className = "fa fa-close remove-filter";
+    div.appendChild(i);
+    container.appendChild(div);
+}
+
+function removeFilter(event){
+    var target = event.target;
+    if (target.tagName === "I"){
+        var id = target.parentNode.id;
+        var filter = id.split('-')[0];
+        var filterName = id.split('-')[1];
+        document.getElementById(filterName.toLowerCase()).checked = false;
+        var index;
+        if (filter === 'Source'){
+            index = source.indexOf(filterName);
+            source.splice(index, 1);
+        } else if (filter === "Area"){
+            index = source.indexOf(filterName);
+            area.splice(index, 1);
+        } else if (filter === "Category"){
+            index = source.indexOf(filterName);
+            category.splice(index, 1);
+        } else if (filter === "Dimension"){
+            index = source.indexOf(filterName);
+            dimension.splice(index, 1);
+        } else if (filter === "Posted") {
+            index = source.indexOf(filterName);
+            posted.splice(index, 1);
+        }
+
+        removeSelectedFilter(id);
+    }
+
+}
+
+function removeSelectedFilter(id){
+    var elm  = document.getElementById(id);
+    elm.parentNode.removeChild(elm);
 }
