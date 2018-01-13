3b1eb9c9783135bc15dc10c0161c2d60f56c16f6
martynas5991 <alex_martynas@yahoo.com>
2018-01-13 17:05:36 +0200

    Added Flickr API filters and functionality

--- GeoS/js/filters.js
+++ GeoS/js/filters.js
@@ -1,6 +1,42 @@
 var filters = [];
 var server = "500px";
 var markers = [];
+var flickrPhotos = [];
+var counter = 0;
+var flickrPlacesLocation = {
+    "21,44,27,48": {
+        "latitude" : "45.946",
+        "longitude" : "24.980"
+    },
+    "-22,36,40,71": {
+        "latitude" : "52.976",
+        "longitude" : "7.857"
+    },
+    "58,0,175,78": {
+        "latitude" : "34.969",
+        "longitude" : "99.819"
+    },
+    "-20,-34,56,36": {
+        "latitude" : "2.070",
+        "longitude" : "15.800"
+    },
+    "-80,-55,-36,15": {
+        "latitude" : "-23.030",
+        "longitude" : "-67.903"
+    },
+    "-163,0,-24,84": {
+        "latitude" : "44.330",
+        "longitude" : "-109.754"
+    },
+    "106,-43,178,0": {
+        "latitude" : "-30.941",
+        "longitude" : "140.810"
+    },
+    "-180,-84,180,-63": {
+        "latitude" : "-81.500",
+        "longitude" : "0.000"
+    }
+};
 function searchPhotos(event) {
     var text = document.getElementById('tag').value;
     if (server === '500px') {
@@ -30,7 +66,7 @@ function request500pxPhotos(tag) {
         requestFilters.was_featured_type = filters['was_featured_type'].value;
     }
     if (tag && tag !== "") {
-        requestFilters.tag = tag;
+        requestFilters.term = tag;
     }
 
     _500px.api('/photos/search', requestFilters, function (response) {
@@ -43,12 +79,11 @@ function request500pxPhotos(tag) {
 }
 
 function displayPhotosFrom500px(photos,source) {
-    console.log(photos);
-    var infowindow = new google.maps.InfoWindow();
     var marker, i,j;
     for (i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
+    markers = [];
     var image = new google.maps.MarkerImage(
         '../GeoS/img/photo-icon.png',
         new google.maps.Size(40, 40),
@@ -56,10 +91,47 @@ function displayPhotosFrom500px(photos,source) {
         new google.maps.Point(20, 20),
         new google.maps.Size(30, 30));
 
+    var ok;
+    //filtering photos by camera and focal length
+    if (filters['camera'] && filters['camera'].value){
+        for ( i = 0; i < photos.length; i++) {
+            if (photos[i].camera!==null){
+                ok = 0;
+                if ((filters['camera'].value[0] && photos[i].camera.indexOf(filters['camera'].value[0]) >= 0) ||
+                    (filters['camera'].value[1] && photos[i].camera.indexOf(filters['camera'].value[1]) >= 0) ||
+                    (filters['camera'].value[2] && photos[i].camera.indexOf(filters['camera'].value[2]) >= 0) ) {
+                    ok = 1;
+                }
+                if (ok === 0) {
+                    photos.splice(i--,1);
+                }
+            } else {
+                photos.splice(i--,1);
+            }
+        }
+    }
+
+    if (filters['focal_length']) {
+        for ( i = 0; i < photos.length; i++) {
+            if (filters['focal_length'].value < parseInt( photos[i].focal_length) || photos[i].focal_length === null  ){
+                photos.splice(i--,1);
+            }
+        }
+    }
+
+    if (filters['geo']) {
+        var location = filters['geo'].value.split(',');
+        map.setCenter({lat:parseFloat(location[0]),lng:parseFloat(location[1])});
+        map.setZoom(3);
+    } else {
+        map.setCenter( {lat:0, lng:30.644});
+        map.setZoom(3);
+    }
+
     for (i = 0, j = 0; i < photos.length; i++) {
 
         if (photos[i].latitude && photos[i].longitude) {
-            markers[j++] = new google.maps.Marker({
+            markers[j] = new google.maps.Marker({
                 position: new google.maps.LatLng(photos[i].latitude, photos[i].longitude),
                 map: map,
                 icon: image
@@ -72,22 +144,132 @@ function displayPhotosFrom500px(photos,source) {
                 photos[i].description = "";
             }
 
-            var links = {image_url: photos[i].image_url, url: photos[i].url};
-            google.maps.event.addListener(markers[i], 'click', (function(marker, i) {
+            google.maps.event.addListener(markers[j], 'click', (function(marker, i) {
                 return function() {
+                    var image_url = "https://500px.com" + photos[i].url;
                     var content = '<b>Source:</b>' + source + '<br>'+
                         '<b>Location:</b>' + photos[i].location_details.city + '<br><br>' +
-                        '<img width="100" src="' + photos[i].image_url + '" onclick="on(\''+ photos[i].image_url  + '\',\'' + photos[i].name + '\',\'' +  photos[i].description + '\',\'' + photos[i].url + '\')" /><br>';
+                        '<img width="100" style="cursor: pointer" src="' + photos[i].image_url + '" onclick="on(\''+ photos[i].image_url  + '\',\'' + photos[i].name + '\',\'' +  photos[i].description + '\',\'' + image_url + '\')" /><br>';
                     infowindow.setContent(content);
                     infowindow.open(map, marker);
                 }
-            })(markers[i], i));
+            })(markers[j++], i));
         }
     }
 }
 
 function requestFlickrPhotos(tag) {
+    console.log(filters);
+    var requestFilters = {};
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c488edfbf44a6cff76eea3da194338cb&format=json&nojsoncallback=1";
 
+    if (filters['bbox'] && filters['bbox'].value) {
+        url = url + "&bbox=" + filters['bbox'].value;
+    }
+    if (filters['geo_context'] && filters['geo_context'].value) {
+        url = url + "&geo_context=" + filters['geo_context'].value;
+    }
+    if (filters['min_taken_date'] && filters['min_taken_date'].value) {
+        url = url + "&min_taken_date=" + filters['min_taken_date'].value;
+    }
+    if (filters['min_upload_date'] && filters['min_upload_date'].value) {
+        url = url + "&min_upload_date=" + filters['min_upload_date'].value;
+    }
+    if (tag && tag !== "") {
+        url = url + "&text=" + tag;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+    request.onloadend = function () {
+        var photos = JSON.parse(request.response).photos;
+        if (photos.total === "0") {
+            alert("No results found");
+        }
+        photos = photos.photo;
+        flickrPhotos = [];
+        for(var i = 0; i < photos.length; i++) {
+            var photoId = photos[i].id;
+            var farm = photos[i].farm;
+            var secret = photos[i].secret;
+            var server = photos[i].server;
+
+            flickrPhotos[photoId] = {};
+            flickrPhotos[photoId].title = photos[i].title;
+            var size = filters['flickr_image_size'] ? filters['flickr_image_size'].value : 'z';
+            flickrPhotos[photoId].url = "https://farm" + farm + ".staticflickr.com/" + server + "/" + photoId + "_" + secret + "_" + size + ".jpg";
+            flickrPhotos[photoId].source = "https://www.flickr.com/photos/" + photos[i].owner + "/" + photoId;
+        }
+        getFlickrPhotosLocation(photos);
+    }
+}
+
+function getFlickrPhotosLocation(photos) {
+    var request;
+    counter = 0;
+    for (var i = 0; i < photos.length; i++) {
+        request = new XMLHttpRequest();
+        request.onloadend = function () {
+            counter ++;
+            if (this.responseText.stat !== 'fail') {
+                var response = JSON.parse(this.responseText);
+                flickrPhotos[response.photo.id].location = response.photo.location;
+            }
+            if (counter === photos.length) {
+                displayPhotosFromFlickr();
+            }
+        };
+        var url = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=c488edfbf44a6cff76eea3da194338cb&format=json&nojsoncallback=1&photo_id=" + photos[i].id;
+        request.open("GET", url);
+        request.send();
+    }
+}
+
+function displayPhotosFromFlickr(){
+    var marker, i,j;
+    for (i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+    google.maps.event.clearListeners(map, 'click');
+    var image = new google.maps.MarkerImage(
+        '../GeoS/img/photo-icon.png',
+        new google.maps.Size(40, 40),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(20, 20),
+        new google.maps.Size(30, 30));
+
+    if (filters['bbox'] && filters['bbox'].value ) {
+        var lat = flickrPlacesLocation[filters['bbox'].value]['latitude'];
+        var lng = flickrPlacesLocation[filters['bbox'].value]['longitude'];
+        map.setCenter({lat:parseFloat(lat),lng:parseFloat(lng)});
+        map.setZoom(3);
+    } else {
+        map.setCenter( {lat:0, lng:30.644});
+        map.setZoom(3);
+    }
+
+    var keys = Object.keys(flickrPhotos);
+    for (i = 0, j = 0; i < keys.length; i++) {
+        if (flickrPhotos[keys[i]].location && flickrPhotos[keys[i]].location.locality) {
+            markers[j] = new google.maps.Marker({
+                position: new google.maps.LatLng(flickrPhotos[keys[i]].location.latitude, flickrPhotos[keys[i]].location.longitude),
+                map: map,
+                icon: image
+            });
+            google.maps.event.addListener(markers[j], 'click', (function(marker, i) {
+                return function() {
+                    var location = flickrPhotos[keys[i]].location.locality._content;
+                    var content = '<b>Source:</b>' + "Flickr" + '<br>'+
+                        '<b>Location:</b>' + location + '<br><br>' +
+                        '<img width="100" style="cursor: pointer" src="' + flickrPhotos[keys[i]].url + '" onclick="on(\''+ flickrPhotos[keys[i]].url  + '\',\'' + flickrPhotos[keys[i]].title + '\',\'' +  "" + '\',\'' + flickrPhotos[keys[i]].source + '\')" /><br>';
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                }
+            })(markers[j++], i));
+        }
+    }
 }
 
 function requestInstagramPhotos(tag) {
@@ -172,6 +354,11 @@ function addFilter(event){
     var filterText = target.getAttribute('data-text');
     if (target.tagName === "INPUT" && target.getAttribute('type') === 'radio'){
         if (target.getAttribute('data-type') === 'unchecked'){
+            var container = document.getElementById(filter);
+            var filterElement = container.querySelector("input[data-type='checked']");
+            if (filterElement) {
+                filterElement.setAttribute('data-type','unchecked');
+            }
             target.setAttribute('data-type','checked');
             if (!filters[filter]){
                 filters[filter] = {};
@@ -180,8 +367,8 @@ function addFilter(event){
             } else if (filters[filter].value !== filterValue) {
                 filters[filter].value = filterValue;
             }
-            var container = document.getElementById("selectedFilters");
-            var filterElement = container.querySelector("div[data-filter='" + filter +"']");
+            container = document.getElementById("selectedFilters");
+            filterElement = container.querySelector("div[data-filter='" + filter +"']");
             if (!filterElement) {
                 addSelectedFilter(filter,filterText);
             } else {
@@ -323,4 +510,33 @@ function openFilters(){
     elm = document.getElementById("open-filters-menu");
     elm.style.display = "none";
 }
+
+function setDateToday(){
+    var elm1 = document.getElementById('todayFlickrUploaded');
+    var elm2 = document.getElementById('todayFlickrTaken');
+    elm1.setAttribute('data-value',new Date());
+    elm2.setAttribute('data-value',new Date());
+}
+
+function setDateWeek(){
+    var elm1 = document.getElementById('lastWeekFlickrUploaded');
+    var elm2 = document.getElementById('lastWeekFlickrTaken');
+    elm1.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*7*1000)))
+    elm2.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*7*1000)))
+}
+
+function setDateMonth(){
+    var elm1 = document.getElementById('lastMonthFlickrUploaded');
+    var elm2 = document.getElementById('lastMonthFlickrTaken');
+    elm1.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*30*1000)))
+    elm2.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*30*1000)))
+}
+
+function setDateYear(){
+    var elm1 = document.getElementById('lastYearFlickrUploaded');
+    var elm2 = document.getElementById('lastYearFlickrTaken');
+    elm1.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*365*1000)))
+    elm2.setAttribute('data-value',new Date(new Date().getTime() - (60*60*24*365*1000)))
+}
+
 
--- GeoS/js/main.js
+++ GeoS/js/main.js
@@ -1,11 +1,17 @@
 
 
 //500px photos
-
+var infowindow;
 window.onload = function () {
+    setDateToday();
+    setDateWeek();
+    setDateMonth();
+    setDateYear();
     _500px.init({
         sdk_key: 'c4e4e7623c6b35f6b42acc1bbb261b685de19d76'
     });
+    infowindow = new google.maps.InfoWindow();
+
 };
 
 
@@ -14,15 +20,15 @@ window.onload = function () {
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 0, lng: 30.644},
+        zoom: 2
     });
 }
 /*----------------------*/
 
 function on(image_url,name,description,url) {
     document.getElementById('fullImage').src = image_url;
-    document.getElementById('fullImageSource').href = "https://500px.com" + url;
+    document.getElementById('fullImageSource').href =  url;
     document.getElementById('fullImageTitle').textContent = name;
     document.getElementById('fullImageDescription').textContent = description;
     document.getElementById("fullscreen").style.display = "block";
